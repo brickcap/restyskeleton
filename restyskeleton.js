ce7e77d2571d155ffafda5447b5b9b0c4299a793
brickcap <akshatjiwan@gmail.com>
2016-09-09 10:02:03 +0530

    My lords commands are being honored

--- restyskeleton.js
+++ restyskeleton.js
@@ -6,14 +6,10 @@ var shell = require("shelljs");
 
 var make_skeleton = function(){
     
-    program.version('0.0.1')        
-	.option('-p, --project', 'Creates an openresty skeleton in the directory name supplied as argument')
-        .action(function(cmd,options){
-	    console.log(cmd);
-	    console.log(options);
-	});
+    program.version('1.0.0')        
+	.option('-p, --project', 'Creates an openresty skeleton in the directory name supplied as argument');
 	program.parse(process.argv);
-    
+    if(program.project) console.log("My lord has commanded me to create a project");
 
 };
 make_skeleton();
