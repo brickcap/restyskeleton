142011c118c238212586176def35cbaf159633ad
brickcap <akshatjiwan@gmail.com>
2016-09-09 13:26:02 +0530

    Has automatic error checking

--- restyskeleton.js
+++ restyskeleton.js
@@ -9,13 +9,12 @@ var make_skeleton = function(){
     program.version('1.0.0');        
     program.option('-d, --directory <dirname>', 'Creates an openresty skeleton in the directory name supplied as argument')
 	.action(function(dirname){
-	    
 	    if (!fs.existsSync(dirname)){
 		try{
 		    fs.mkdirSync(dirname);
 		    console.log("Openresty app is ready in:"+dirname+"" );
 		}
-		catch(excep){
+		catch(ex){
 		    console.log("Can't create a project in " +dirname);
 		}
 	    }
